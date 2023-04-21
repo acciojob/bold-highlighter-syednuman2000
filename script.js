6a22a027a7e1145868d62fbf3efeaf8e40b6a9b4
Syed Numan <111847221+syednuman2000@users.noreply.github.com>
2023-04-21 19:36:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function highlight() {
     //Write your code here
-
-
+	let bold = document.getElementByTagName("strong");
+	for(let i=0;i<bold.length;i++){
+		bold[i].style.color = "green";
+	}
 }
-
-
 function return_normal() {
     //Write your code here
-
-    
+	let bold = document.getElementByTagName("strong");
+	for(let i=0;i<bold.length;i++){
+		bold[i].style.color = "black";
+	}
 }
